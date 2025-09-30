19fb59b49b5a104f26187dcba30b8ec4bf2252c5
Avery Keener <averyjkeener@gmail.com>
2025-09-30 15:12:29 -0700

    cleaned up imports, added useEffect for likes

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react'
 
-// States demo
-// States are not persistent across page reloads
+
+// useEffect demo
+// For tracking likes
 const Card = ({ title }) => {
-  // add a "Liked" state
   const [hasLiked, setHasLiked] = useState(false);
+
+  useEffect(() => {
+  console.log(`${title} has been liked: ${hasLiked}`);
+});
+
 
   return(
     <div className="card" >
       <h2>{title}</h2>
 
-      // liked toggle
       <button onClick={() => setHasLiked(!hasLiked)}>
         {hasLiked ? 'Liked' : 'Like'}
       </button>
     </div>
   )
 }
-
-// if liked was passed as a Prop, cards would not be able to have an individual liked value
 
 const App = () => {
   
